5e2ae8e9e02ac0134b6e240bf22862de0ea60071
chaituvarma-109 <chaitanyavarma.u@gmail.com>
2023-07-11 15:35:49 +0530

    completed the web app

--- main.py
+++ main.py
@@ -1,1 +1,84 @@
-print("hi")
+import streamlit as st
+
+from storesales.logger import logging
+from storesales.pipeline.predict_pipeline import CustomData, PredictPipeline
+
+fat_content = ['Low Fat', 'Regular']
+item_type = ['Food', 'Drinks', 'Non-Consumable']
+outlet_size = ['Medium', 'Small', 'High']
+outlet_location_type = ['Tier 1', 'Tier 2', 'Tier 3']
+outlet_type = ['Supermarket Type1', 'Supermarket Type2', 'Grocery Store', 'Supermarket Type3']
+
+st.set_page_config(page_title="Store Sales Prediction App", page_icon="🧊", layout="wide", )
+
+st.header('Prediction Form')
+with st.form("my_form"):
+    col1, col2, col3 = st.columns(3)
+    col4, col5, col6 = st.columns(3)
+    col7, col8, col9 = st.columns(3)
+    col10, col11 = st.columns(2)
+
+    with col1:
+        Item_Identifier = st.text_input('Item_Identifier')
+
+    with col2:
+        Item_Weight = st.number_input('Item_Weight')
+
+    with col3:
+        Item_Fat_Content = st.selectbox('Item_Fat_Content', fat_content)
+
+    with col4:
+        Item_Visibility = st.number_input('Item_Visibility')
+
+    with col5:
+        Item_Type = st.selectbox('Item_Type', item_type)
+
+    with col6:
+        Item_MRP = st.number_input('Item_MRP')
+
+    with col7:
+        Outlet_Identifier = st.text_input('Outlet_Identifier')
+
+    with col8:
+        Outlet_Establishment_Year = st.number_input('Outlet_Establishment_Year')
+
+    with col9:
+        Outlet_Size = st.selectbox('Outlet_Size', outlet_size)
+
+    with col10:
+        Outlet_Location_Type = st.selectbox('Outlet_Location_Type', outlet_location_type)
+
+    with col11:
+        Outlet_Type = st.selectbox('Outlet_Type', outlet_type)
+
+    # Every form must have a submit button.
+    predict = st.form_submit_button("Predict Item Outlet Sale", type="primary")
+    if predict:
+        sales_data = CustomData(Item_Identifier=Item_Identifier,
+                                Item_Weight=Item_Weight,
+                                Item_Fat_Content=Item_Fat_Content,
+                                Item_Visibility=Item_Visibility,
+                                Item_Type=Item_Type,
+                                Item_MRP=Item_MRP,
+                                Outlet_Identifier=Outlet_Identifier,
+                                Outlet_Establishment_Year=Outlet_Establishment_Year,
+                                Outlet_Size=Outlet_Size,
+                                Outlet_Location_Type=Outlet_Location_Type,
+                                Outlet_Type=Outlet_Type,
+                                )
+
+        logging.info("getting sales data as dataframe")
+        sales_df = sales_data.get_data_as_data_frame()
+
+        logging.info(">>>>>>>>> Prediction started >>>>>>>>>")
+
+        predict_pipeline = PredictPipeline()
+
+        logging.info("Mid Prediction")
+        results = predict_pipeline.predict(sales_df)
+
+        st.write(f"Prediction result is: {results[0]}")
+
+        logging.info("after Prediction")
+        logging.info(f"results: {results}")
+        logging.info(">>>>>>>>> Prediction Completed >>>>>>>>>")
--- storesales/pipeline/predict_pipeline.py
+++ storesales/pipeline/predict_pipeline.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 from storesales.exception import CustomException
 from storesales.utils import load_object
+from storesales.logger import logging
 
 
 class PredictPipeline:
@@ -15,10 +16,10 @@ class PredictPipeline:
         try:
             model_path = os.path.join("artifacts", "model.pkl")
             preprocessor_path = os.path.join('artifacts', 'preprocessor.pkl')
-            print("Before Loading")
+            logging.info("Before Loading")
             model = load_object(file_path=model_path)
             preprocessor = load_object(file_path=preprocessor_path)
-            print("After Loading")
+            logging.info("After Loading")
             data_scaled = preprocessor.transform(features)
             preds = model.predict(data_scaled)
             return preds
@@ -29,38 +30,55 @@ class PredictPipeline:
 
 class CustomData:
     def __init__(self,
-                 gender: str,
-                 race_ethnicity: str,
-                 parental_level_of_education,
-                 lunch: str,
-                 test_preparation_course: str,
-                 reading_score: int,
-                 writing_score: int):
+                 Item_Identifier: str,
+                 Item_Weight: int,
+                 Item_Fat_Content: str,
+                 Item_Visibility: int,
+                 Item_Type: str,
+                 Item_MRP: int,
+                 Outlet_Identifier: str,
+                 Outlet_Establishment_Year: int,
+                 Outlet_Size: str,
+                 Outlet_Location_Type: str,
+                 Outlet_Type: str
+                 ):
 
-        self.gender = gender
+        self.Item_Identifier = Item_Identifier
 
-        self.race_ethnicity = race_ethnicity
+        self.Item_Weight = Item_Weight
 
-        self.parental_level_of_education = parental_level_of_education
+        self.Item_Fat_Content = Item_Fat_Content
 
-        self.lunch = lunch
+        self.Item_Visibility = Item_Visibility
 
-        self.test_preparation_course = test_preparation_course
+        self.Item_Type = Item_Type
 
-        self.reading_score = reading_score
+        self.Item_MRP = Item_MRP
 
-        self.writing_score = writing_score
+        self.Outlet_Identifier = Outlet_Identifier
+
+        self.Outlet_Establishment_Year = Outlet_Establishment_Year
+
+        self.Outlet_Size = Outlet_Size
+
+        self.Outlet_Location_Type = Outlet_Location_Type
+
+        self.Outlet_Type = Outlet_Type
 
     def get_data_as_data_frame(self):
         try:
             custom_data_input_dict = {
-                "gender": [self.gender],
-                "race_ethnicity": [self.race_ethnicity],
-                "parental_level_of_education": [self.parental_level_of_education],
-                "lunch": [self.lunch],
-                "test_preparation_course": [self.test_preparation_course],
-                "reading_score": [self.reading_score],
-                "writing_score": [self.writing_score],
+                "Item_Identifier": [self.Item_Identifier],
+                "Item_Weight": [self.Item_Weight],
+                "Item_Fat_Content": [self.Item_Fat_Content],
+                "Item_Visibility": [self.Item_Visibility],
+                "Item_Type": [self.Item_Type],
+                "Item_MRP": [self.Item_MRP],
+                "Outlet_Identifier": [self.Outlet_Identifier],
+                "Outlet_Establishment_Year": [self.Outlet_Establishment_Year],
+                "Outlet_Size": [self.Outlet_Size],
+                "Outlet_Location_Type": [self.Outlet_Location_Type],
+                "Outlet_Type": [self.Outlet_Type]
             }
 
             return pd.DataFrame(custom_data_input_dict)
