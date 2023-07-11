269600f8407957d0de0c4900ab7a637ab034f318
chaituvarma-109 <chaitanyavarma.u@gmail.com>
2023-07-11 15:36:58 +0530

    modified logger and requirements file

--- storesales/logger.py
+++ storesales/logger.py
@@ -2,11 +2,12 @@ import logging
 import os
 from datetime import datetime
 
-LOG_DIR = '../../Logs'
+LOG_DIR = 'Logs'
 LOG_FILE = f"log_{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-os.makedirs(LOG_DIR, exist_ok=True)
+LOGS_PATH = os.path.join(os.getcwd(), LOG_DIR, LOG_FILE)
+os.makedirs(LOGS_PATH, exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
+LOG_FILE_PATH = os.path.join(LOGS_PATH, LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
