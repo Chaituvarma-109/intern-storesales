df86088f2816ec52d582af8e78e30d4014ab0693
chaituvarma-109 <chaitanyavarma.u@gmail.com>
2023-07-12 22:47:10 +0530

    rewritten training pipeline and written configuration files

---
+++ sales/entity/config_entity.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    data_path: Path
+    train_data_path: Path
+    test_data_path: Path
+
+
+@dataclass(frozen=True)
+class DataTransformationConfig:
+    root_dir: Path
+    preprocessed_obj_file_path: Path
+
+
+@dataclass(frozen=True)
+class ModelTrainerConfig:
+    root_dir: Path
+    model_file_path: Path
